4ea034af2597a1a9a1069957b6a951e80356aba4
Marek Ulicny <xulicny@gmail.com>
2020-04-18 13:10:35 +0200

    Changed: Split Experiences into Experience components.

---
+++ src/components/Experience.vue
@@ -0,0 +1,181 @@
+<template>
+  <div :key="experience.id" class="experience">
+    <h2>{{ experience.datetime | formatDateAndTime }}</h2>
+
+    <h5>
+      {{ experience.datetime | dayInWeek }},
+      {{ experience.datetime | asIntervalDate }}
+    </h5>
+
+    <!-- TODO: Create Experience class with helper methods (datetime, hasSituation, hasSolution,...) -->
+
+    <div>
+      <div
+        v-if="
+          experience.situationStory !== '' ||
+            experience.situationActivities.length > 0 ||
+            experience.situationEmotions.length > 0
+        "
+        class="situation"
+      >
+        <h3>Situace</h3>
+
+        <div v-if="experience.situationStory !== ''" class="story">
+          {{ experience.situationStory }}
+        </div>
+
+        <div v-if="experience.situationActivities.length > 0" class="tags">
+          <h4>Aktivity:</h4>
+
+          <ul>
+            <li
+              v-for="activity in experience.situationActivities"
+              :key="activity"
+            >
+              <tag :title="activity" type="negative" />
+            </li>
+          </ul>
+        </div>
+
+        <div v-if="experience.situationEmotions.length > 0" class="tags">
+          <h4>Emoce:</h4>
+
+          <ul>
+            <li v-for="emotion in experience.situationEmotions" :key="emotion">
+              <tag :title="emotion" type="negative" />
+            </li>
+          </ul>
+        </div>
+      </div>
+
+      <hr
+        v-if="
+          experience.solutionStory !== '' ||
+            experience.solutionActivities.length > 0 ||
+            experience.solutionEmotions.length > 0
+        "
+      />
+
+      <div
+        v-if="
+          experience.solutionStory !== '' ||
+            experience.solutionActivities.length > 0 ||
+            experience.solutionEmotions.length > 0
+        "
+        class="solution"
+      >
+        <h3>Řešení</h3>
+
+        <div>
+          <div v-if="experience.solutionStory !== ''" class="story">
+            {{ experience.solutionStory }}
+          </div>
+
+          <div v-if="experience.solutionActivities.length > 0" class="tags">
+            <h4>Aktivity:</h4>
+
+            <ul>
+              <li
+                v-for="activity in experience.solutionActivities"
+                :key="activity"
+              >
+                <tag :title="activity" type="positive" />
+              </li>
+            </ul>
+          </div>
+
+          <div v-if="experience.solutionEmotions.length > 0" class="tags">
+            <h4>Emoce:</h4>
+
+            <ul>
+              <li v-for="emotion in experience.solutionEmotions" :key="emotion">
+                <tag :title="emotion" type="positive" />
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue'
+
+import Tag from '@/components/Tag.vue'
+import { Experience } from '@/store/experiences/types'
+
+export default Vue.extend({
+  name: 'Experience',
+
+  components: {
+    Tag,
+  },
+
+  props: {
+    experience: {
+      type: Object as PropType<Experience>,
+      required: true,
+    },
+  },
+})
+</script>
+
+<style lang="scss" scoped>
+.experience {
+  width: 40rem;
+  border: 1px solid #acacac;
+  box-sizing: border-box;
+  border-radius: 0.5rem;
+  padding: 1.5rem;
+}
+
+.story {
+  font-family: 'Martel', serif;
+  font-size: 0.85rem;
+  font-weight: 600;
+  margin: 1.5em 0.5em;
+}
+
+.tags {
+  margin: 0.2em 0;
+}
+
+hr {
+  margin: 1.3em 2em 1.1em 2em;
+}
+
+ul {
+  list-style: none;
+  display: inline;
+  margin: 0;
+  padding: 0;
+  font-size: 0.9em;
+
+  li {
+    display: inline-block;
+  }
+}
+
+h2 {
+  font-size: 1.2em;
+  margin: 0;
+  font-weight: bolder;
+}
+
+h3 {
+  font-size: 1em;
+  margin: 0.3em 0;
+}
+
+h4 {
+  display: inline;
+  font-size: 0.9em;
+  margin-right: 0.5em;
+}
+
+h5 {
+  margin: 0.3em 0 1.5em 0;
+  color: #acacac;
+}
+</style>
--- src/components/Experiences.vue
+++ src/components/Experiences.vue
@@ -8,128 +8,24 @@
       <i class="fas fa-plus"></i>
     </router-link>
 
-    <div
+    <experience
       v-for="experience in experiences"
       :key="experience.id"
-      class="experience"
-    >
-      <h2>{{ experience.datetime | formatDateAndTime }}</h2>
-
-      <h5>
-        {{ experience.datetime | dayInWeek }},
-        {{ experience.datetime | asIntervalDate }}
-      </h5>
-
-      <!-- TODO: Create Experience class with helper methods (datetime, hasSituation, hasSolution,...) -->
-      <!-- TODO: Rename `solution` to `solutionStory` -->
-
-      <div>
-        <div
-          v-if="
-            experience.situationStory !== '' ||
-              experience.situationActivities.length > 0 ||
-              experience.situationEmotions.length > 0
-          "
-          class="situation"
-        >
-          <h3>Situace</h3>
-
-          <div v-if="experience.situationStory !== ''" class="story">
-            {{ experience.situationStory }}
-          </div>
-
-          <div v-if="experience.situationActivities.length > 0" class="tags">
-            <h4>Aktivity:</h4>
-
-            <ul>
-              <li
-                v-for="activity in experience.situationActivities"
-                :key="activity"
-              >
-                <tag :title="activity" type="negative" />
-              </li>
-            </ul>
-          </div>
-
-          <div v-if="experience.situationEmotions.length > 0" class="tags">
-            <h4>Emoce:</h4>
-
-            <ul>
-              <li
-                v-for="emotion in experience.situationEmotions"
-                :key="emotion"
-              >
-                <tag :title="emotion" type="negative" />
-              </li>
-            </ul>
-          </div>
-        </div>
-
-        <hr
-          v-if="
-            experience.solutionStory !== '' ||
-              experience.solutionActivities.length > 0 ||
-              experience.solutionEmotions.length > 0
-          "
-        />
-
-        <div
-          v-if="
-            experience.solutionStory !== '' ||
-              experience.solutionActivities.length > 0 ||
-              experience.solutionEmotions.length > 0
-          "
-          class="solution"
-        >
-          <h3>Řešení</h3>
-
-          <div>
-            <div v-if="experience.solutionStory !== ''" class="story">
-              {{ experience.solutionStory }}
-            </div>
-
-            <div v-if="experience.solutionActivities.length > 0" class="tags">
-              <h4>Aktivity:</h4>
-
-              <ul>
-                <li
-                  v-for="activity in experience.solutionActivities"
-                  :key="activity"
-                >
-                  <tag :title="activity" type="positive" />
-                </li>
-              </ul>
-            </div>
-
-            <div v-if="experience.solutionEmotions.length > 0" class="tags">
-              <h4>Emoce:</h4>
-
-              <ul>
-                <li
-                  v-for="emotion in experience.solutionEmotions"
-                  :key="emotion"
-                >
-                  <tag :title="emotion" type="positive" />
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
-    </div>
+      :experience="experience"
+    />
   </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
 
-import Tag from '@/components/Tag.vue'
+import Experience from '@/components/Experience.vue'
 
 export default Vue.extend({
   name: 'Experiences',
 
   components: {
-    Tag,
+    Experience,
   },
 
   props: {
@@ -155,62 +51,5 @@ export default Vue.extend({
       margin: 1em 0 0.5em 0;
     }
   }
-}
-
-.experience {
-  width: 40rem;
-  border: 1px solid #acacac;
-  box-sizing: border-box;
-  border-radius: 0.5rem;
-  padding: 1.5rem;
-}
-
-.story {
-  font-family: 'Martel', serif;
-  font-size: 0.85rem;
-  font-weight: 600;
-  margin: 1.5em 0.5em;
-}
-
-.tags {
-  margin: 0.2em 0;
-}
-
-hr {
-  margin: 1.3em 2em 1.1em 2em;
-}
-
-ul {
-  list-style: none;
-  display: inline;
-  margin: 0;
-  padding: 0;
-  font-size: 0.9em;
-
-  li {
-    display: inline-block;
-  }
-}
-
-h2 {
-  font-size: 1.2em;
-  margin: 0;
-  font-weight: bolder;
-}
-
-h3 {
-  font-size: 1em;
-  margin: 0.3em 0;
-}
-
-h4 {
-  display: inline;
-  font-size: 0.9em;
-  margin-right: 0.5em;
-}
-
-h5 {
-  margin: 0.3em 0 1.5em 0;
-  color: #acacac;
 }
 </style>
