4734fe72ccd2a30b9f7f2a21d1f0e9a7be9c3e29
Marek Ulicny <xulicny@gmail.com>
2020-04-27 13:59:01 +0200

    Fixed: Change Webpack config so that it places source maps into `webpack-debug-ready` folder in Chrome Dev Tools.

--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,21 @@
 module.exports = {
   lintOnSave: false,
+
+  // This places sources in `webpack-debug-ready` folder. Otherwise there are many files with similar names.
+  // See https://github.com/vuejs/vue-cli/issues/2978 for details.
+  configureWebpack: config => {
+    if (process.env.NODE_ENV === 'development') {
+      config.devtool = 'eval-source-map'
+
+      config.output.devtoolFallbackModuleFilenameTemplate =
+        'webpack:///[resource-path]?[hash]'
+
+      config.output.devtoolModuleFilenameTemplate = info => {
+        // In my case this condition filtered my original files
+        return info.moduleId === ''
+          ? `webpack-debug-ready:///${info.resourcePath}`
+          : `webpack-generated:///${info.resourcePath}?${info.hash}`
+      }
+    }
+  },
 }
